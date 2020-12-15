99d39b054d3ff0bb0b0044b09d0c263159690dba
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2020-12-15 10:50:17 +0100

    Update test.js

--- javascripts/test.js
+++ javascripts/test.js
@@ -1,6 +1,6 @@
 (function(){
   console.log("load test script");
-  var location = document.location;*
+  var location = document.location;
   localStorage.setItem("foo","bar");
   localStorage.setItem("location",  JSON.stringify(location));
   console.log("localstorage[xxx]:", localStorage.getItem("xxx"));
