d2cbffa5a735ba9faa42342718e8ef5ddf5f7efd
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2020-12-15 10:36:40 +0100

    Create test.js

---
+++ javascripts/test.js
@@ -0,0 +1,7 @@
+(function(){
+  console.log("load test script");
+  var location = document.location;*
+  localStorage.setItem("foo","bar");
+  localStorage.setItem("location",  JSON.stringify(location));
+  console.log("localstorage[xxx]:", localStorage.getItem("xxx"));
+}())
